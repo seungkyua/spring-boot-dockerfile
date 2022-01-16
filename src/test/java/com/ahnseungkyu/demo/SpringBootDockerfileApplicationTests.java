984bd040f6ba544e3ede8300f1bfa107c3ca6ddc
Seungkyu Ahn <seungkyua@gmail.com>
2022-01-16 22:35:11 +0900

    Initial commit

---
+++ src/test/java/com/ahnseungkyu/demo/SpringBootDockerfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahnseungkyu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
